a1095e766d84b5903dd3c904bee1bd0a623c59ba
garkulic <garkul.aa@phystech.edu>
2023-03-13 20:07:28 +0300

    add test for delete_all

--- tests/test_repository/test_sqlite_repository.py
+++ tests/test_repository/test_sqlite_repository.py
@@ -7,61 +7,62 @@ from datetime import datetime
 from bookkeeper.repository.sqlite_repository import SqliteRepository
 
 DB_FILE = "test4sql_repository.db"
-TEST_INT_VALUE = 123
+TEST_INT_VALUE = 73
 TEST_STR_VALUE = "test sring"
-
-@pytest.fixture
-def create_bd():
-    with sqlite3.connect(DB_FILE) as con:
-        cur = con.cursor()
-        cur.execute(f"DROP TABLE IF EXISTS custom")
-    with sqlite3.connect(DB_FILE) as con:
-        cur = con.cursor()
-        cur.execute(f"CREATE TABLE IF NOT EXISTS custom(f1 int, f2 srt)")
-    con.close()
 
 @pytest.fixture
 def custom_class():
     @dataclass
     class Custom():
-        f1: int = TEST_INT_VALUE
-        f2: str = TEST_STR_VALUE
+        int_value: int = TEST_INT_VALUE
+        str_value: str = TEST_STR_VALUE
+        date_value: str = str(datetime.datetime.now())
         pk: int = 0
     return Custom
 
 @pytest.fixture
+def drop_table():
+    with sqlite3.connect(DB_FILE) as con:
+        cur = con.cursor()
+        cur.execute("DROP TABLE IF EXISTS custom")
+
+@pytest.fixture
+def create_bd():
+    with sqlite3.connect(DB_FILE) as con:
+        cur = con.cursor()
+        cur.execute(f"CREATE TABLE IF NOT EXISTS custom(int_value int, str_value srt)")
+    con.close()
+
+@pytest.fixture
 def repo(custom_class, create_bd):
-    return SQLiteRepository(db_file=DB_FILE, cls=custom_class)
-
-def test_row2obj(repo):
-    row = (123, "test__string_row2obj")
-    obj = repo._row2obj(10, row)
-    assert obj.pk == 10
-    assert obj.f1 == 123
-    assert obj.f2 == "test_string_row2obj"
-
+    return SqliteRepository(db_file=DB_FILE, cls=custom_class)
 
 def test_crud(repo, custom_class):
+    # create object
+    obj = custom_class()
     # add
-    obj_add = custom_class()
-    pk = repo.add(obj_add)
-    assert pk == obj_add.pk
+    pk = repo.add(obj)
+    assert pk == obj.pk
     # get
+    assert repo.get(pk) is not None
     obj_get = repo.get(pk)
-    assert obj_get is not None
-    assert obj_get.pk == obj_add.pk
-    assert obj_get.f1 == obj_add.f1
-    assert obj_get.f2 == obj_add.f2
+    assert repo.get(pk) == obj #проверяем, что по индексу pk верно получаем объект
+    assert obj_get.int_value == obj.int_value
+    assert obj_get.str_value == obj.str_value
+    assert obj_get.date_value == obj.date_value
+    
     # update
-    obj_upd = custom_class(f1=10, f2="test_string_crud", pk=pk)
-    repo.update(obj_upd)
-    obj_get = repo.get(pk)
-    assert obj_get == obj_upd
-    assert obj_get_upd.field_str == obj_upd.f1
+    obj2 = custom_class(int_value = 37, str_value = "test str2", date_value = "1999-10-04 07:37:00" )
+    obj2.pk = pk
+    repo.update(obj2)
+    assert repo.get(pk) == obj2
+    assert obj. == obj_upd.f1
     assert obj_get_upd.field_int == obj_upd.f2
+    
     # delete
     repo.delete(pk)
     assert repo.get(pk) is None
+    repo.delete_all()
 
 def test_cannot_add_with_pk(repo, custom_class):
     obj = custom_class()
@@ -78,12 +79,12 @@ def test_cannot_delete_unexistent(repo):
         repo.delete(-1)
 
 def test_cannot_update_unexistent(repo, custom_class):
-    obj = custom_class(f1=1, pk=100)
+    obj = custom_class()
     with pytest.raises(ValueError):
         repo.update(obj)
 
 def test_cannot_update_without_pk(repo, custom_class):
-    obj = custom_class(f1=1, pk=0)
+    obj = custom_class()
     with pytest.raises(ValueError):
         repo.update(obj)
 
@@ -91,6 +92,7 @@ def test_get_unexistent(repo):
     assert repo.get(-1) is None
 
 def test_get_all(repo, custom_class):
+    repo.delete_all()
     objects = [custom_class() for i in range(5)]
     for o in objects:
         repo.add(o)
@@ -99,21 +101,30 @@ def test_get_all(repo, custom_class):
 def test_get_all_with_condition(repo, custom_class):
     objects = []
     for i in range(5):
-        o = custom_class(f1=1)
-        o.f1 = 1
-        o.f2 = 'test'
+        o = custom_class()
+        o.int_value = i
+        o.str_value = 'test string'
         repo.add(o)
         objects.append(o)
-    assert [objects[0]] == repo.get_all({'f1': 0})
-    assert objects == repo.get_all({'f2': 'test'})
+    assert [objects[0]] == repo.get_all({'int_value': 0})
+    assert objects == repo.get_all({'str_value': 'test string'})
 
 def test_get_all_like(repo, custom_class):
     objects = []
     for i in range(5):
-        o = custom_class(f1=1)
-        o.f1 = "__" + str(i) + "__"
-        o.f2 = 'test'
+        o = custom_class()
+        o.int_value = "__" + str(i) + "__"
+        o.str_value = 'test string'
         repo.add(o)
         objects.append(o)
-    assert [objects[0]] == repo.get_all_like({'f1': '0'})
-    assert objects == repo.get_all_like({'f2': 'test'})
+    assert [objects[0]] == repo.get_all_like({'int_value': '0'})
+    assert objects == repo.get_all_like({'str_value': 'test srting'})
+
+def test_delete_all(repo, custom_class):
+    objects = []
+    objects = [custom_class() for i in range(5)]
+    for o in objects:
+        repo.add(o)
+    repo.delete_all()
+    assert repo.get_all() == None
+
